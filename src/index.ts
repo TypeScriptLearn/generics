ec19214a127f91bf6945431300d8c19aff3fe952
Valery Semenenko <gearmobile@gmail.com>
2021-01-25 21:25:42 +0300

    add tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ function someGeneric<T>(value: T): T {
     return value;
 }
 
-console.log(someGeneric('apple').length)
-console.log(someGeneric(12))
+console.log(someGeneric('apple').length);
+console.log(someGeneric(12));
+
+const numbers: Array<number> = [1,2,3,4,5];
